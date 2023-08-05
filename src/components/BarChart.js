20b3a858fdf64e8da7e02145e81fa4464b0b3c2f
Harshit Dubey <hdlikes8@gmail.com>
2023-08-06 00:49:16 +0530

    removing warnings

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,4 +1,4 @@
-import { Bar, Scatter } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 import { styled } from "styled-components";
 export const BarChart = ({ chartData }) => {
   let delayed;
